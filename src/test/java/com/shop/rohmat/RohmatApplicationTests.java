21787257955e17bb6a071e0e42bb6c045fdae44a
Rohmat <rohmathidayatyusuf@gmail.com>
2026-03-10 20:08:50 +0700

    rohmatapp

---
+++ src/test/java/com/shop/rohmat/RohmatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.rohmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohmatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
