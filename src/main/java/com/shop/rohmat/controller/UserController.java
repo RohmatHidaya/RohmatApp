3ccacc035c1125f0604cf6fc3e2497e56b1fd4e6
Rohmat <rohmathidayatyusuf@gmail.com>
2026-03-16 08:24:13 +0700

    Add Lombok dependency and refactor User model and UserController for improved code simplicity and functionality

--- src/main/java/com/shop/rohmat/controller/UserController.java
+++ src/main/java/com/shop/rohmat/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.shop.rohmat.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,25 +14,46 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shop.rohmat.model.User;
 import com.shop.rohmat.repository.UserRepository;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
+@RequestMapping(path = "/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @GetMapping(path = "/users")
+    @GetMapping
     public @ResponseBody Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @PostMapping(path = "/add")
-    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email,
+            @RequestParam String password) {
         User n = new User();
         n.setName(name);
         n.setEmail(email);
+        n.setPassword(password);
         userRepository.save(n);
         return "Saved";
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<User> GetUserById(@PathVariable Integer id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Integer id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.delete(user);
+        return ResponseEntity.ok("Deleted");
     }
 }
--- src/main/java/com/shop/rohmat/model/User.java
+++ src/main/java/com/shop/rohmat/model/User.java
@@ -6,7 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class User {
     @Id
@@ -15,28 +19,5 @@ public class User {
 
     private String name;
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String password;
 }
